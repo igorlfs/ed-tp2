8693ac4e27defc73f60dd8f0bdc94c299f6d8fe6
Igor <igorlfs@ufmg.br>
2022-01-10 06:53:24 -0300

    refactor(fita): encapsulation

--- include/fita.hpp
+++ include/fita.hpp
@@ -27,12 +27,12 @@ inline bool operator>=(const Page &lhs, const Page &rhs) {
 
 class Fita {
   public:
-    void read(Page *p, int &n) const;
-    void quickSort(Page *p, const int &n) { sort(0, n - 1, p); }
-    void write(Page *p, const int &n) const;
     void sortFitas(int &n);
 
   private:
+    void read(Page *p, int &n) const;
+    void quickSort(Page *p, const int &n) { sort(0, n - 1, p); }
+    void write(Page *p, const int &n) const;
     void sort(const int &left, const int &right, Page *p) const;
     void partition(const int &left, const int &right, int *i, int *j,
                    Page *p) const;
